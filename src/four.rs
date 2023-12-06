a15f3906173468134822d4554b423828f96f9c2e
hafrei <h.a.freiburger@gmail.com>
2023-12-05 20:49:32 -0500

    Refactor day four a bit

--- src/four.rs
+++ src/four.rs
@@ -2,10 +2,23 @@ struct Card {
     id: u32,
     winning: Vec<u32>,
     play: Vec<u32>,
+    id_links: Vec<u32>,
+    point_value: Option<u32>,
 }
 
 impl Card {
-    fn count_points(&self) -> u32 {
+
+    fn new(id: u32, winning: Vec<u32>, play: Vec<u32>) -> Self {
+        Self {
+            id,
+            winning,
+            play,
+            id_links: vec![],
+            point_value: None
+        }
+    }
+
+    fn get_point_count(&self) -> u32 {
         println!("On Card {}", self.id);
         let mut tally: u32 = 0;
         let mut value = 0;
@@ -33,9 +46,26 @@ impl Card {
         println!("     this card is worth {value} total points\n");
         value
     }
+
+    fn part_two_count_points(&mut self) {
+        self.point_value = Some(self.get_point_count());
+    }
+
+    fn part_one_count_points(&self) -> u32 {
+        self.get_point_count()
+    }
 }
 
 pub fn run(input: String) {
+    let mut cards: Vec<Card> = get_cards(input);
+    let part_one: u32 = cards.iter().map(|x| x.part_one_count_points()).sum();
+    println!("The cards are worth {part_one} points");
+    cards.iter_mut().map(|x| {x.part_two_count_points(); x});
+    let part_one: u32 = cards.iter().filter(|x| x.point_value.is_some()).map(|x| x.point_value.unwrap()).sum();
+    println!("The cards are worth {part_one} points");
+}
+
+fn get_cards(input: String) -> Vec<Card> {
     let mut cards: Vec<Card> = Vec::new();
     for card in input.lines() {
         let (card_enum, numbers) = card.split_once(':').unwrap();
@@ -56,8 +86,7 @@ pub fn run(input: String) {
             .split_whitespace()
             .map(|x| x.parse::<u32>().unwrap())
             .collect::<Vec<u32>>();
-        cards.push(Card {id, winning, play});
+        cards.push(Card::new(id, winning, play));
     }
-    let part_one: u32 = cards.iter().map(|x| x.count_points()).sum();
-    println!("The cards are worth {part_one} points");
+    cards
 }
