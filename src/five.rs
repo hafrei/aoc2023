3fbb92e85fdf48900bb1c58a9b44c347f7ae665a
hafrei <h.a.freiburger@gmail.com>
2023-12-16 16:08:06 -0500

    Day 5 part one, and a silly slow day two implementation which is too slow to process part two on the test input

---
+++ src/five.rs
@@ -0,0 +1,168 @@
+use rayon::prelude::*;
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Mapping {
+    SeedToSoil,
+    SoilToFertilizer,
+    FertilizerToWater,
+    WaterToLight,
+    LightToTemperature,
+    TemperatureToHumidity,
+    HumidityToLocation,
+    Finished,
+}
+
+impl Mapping {
+    pub fn determine(input: &str) -> Self {
+        if input.contains("seed") & input.contains("soil") {
+            Mapping::SeedToSoil
+        } else if input.contains("soil") & input.contains("fertilizer") {
+            Mapping::SoilToFertilizer
+        } else if input.contains("fertilizer") & input.contains("water") {
+            Mapping::FertilizerToWater
+        } else if input.contains("water") & input.contains("light") {
+            Mapping::WaterToLight
+        } else if input.contains("light") & input.contains("temperature") {
+            Mapping::LightToTemperature
+        } else if input.contains("temperature") & input.contains("humidity") {
+            Mapping::TemperatureToHumidity
+        } else {
+            Mapping::HumidityToLocation
+        }
+    }
+    pub fn next(&self) -> Self {
+        match self {
+            Mapping::SeedToSoil => Mapping::SoilToFertilizer,
+            Mapping::SoilToFertilizer => Mapping::FertilizerToWater,
+            Mapping::FertilizerToWater => Mapping::WaterToLight,
+            Mapping::WaterToLight => Mapping::LightToTemperature,
+            Mapping::LightToTemperature => Mapping::TemperatureToHumidity,
+            Mapping::TemperatureToHumidity => Mapping::HumidityToLocation,
+            Mapping::HumidityToLocation => Mapping::Finished,
+            Mapping::Finished => unreachable!(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Page {
+    mapping: Mapping,
+    destination_end: u64,
+    source_start: u64,
+    source_end: u64,
+}
+
+impl Page {
+    pub fn destination_value(&self, input: u64) -> Option<u64> {
+        if input >= self.source_start && input <= self.source_end {
+            let range = self.source_end - input;
+            Some(self.destination_end - range)
+        } else {
+            None
+        }
+    }
+}
+
+struct Almanac {
+    pages: Vec<Page>,
+    seeds: Vec<u64>,
+}
+
+impl Almanac {
+    pub fn new() -> Self {
+        Self {
+            pages: Vec::new(),
+            seeds: Vec::new(),
+        }
+    }
+
+    pub fn fill(&mut self, input: String) {
+        let mut current_mapping = Mapping::SeedToSoil;
+        for lin in input.lines() {
+            if lin.is_empty() {
+                continue;
+            } else if lin.contains("seeds:") {
+                let Some((_, nums)) = lin.split_once(' ') else {
+                    unreachable!()
+                };
+                self.seeds.append(
+                    &mut nums
+                        .trim()
+                        .split_whitespace()
+                        .map(|x| x.parse::<u64>().unwrap())
+                        .collect::<Vec<u64>>(),
+                );
+            } else if lin.chars().all(|c| !c.is_numeric()) {
+                current_mapping = Mapping::determine(lin);
+            } else {
+                let parsing = lin
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse::<u64>().unwrap())
+                    .collect::<Vec<u64>>();
+                let page = Page {
+                    mapping: current_mapping,
+                    destination_end: parsing[0] + parsing[2] - 1,
+                    source_start: parsing[1],
+                    source_end: parsing[1] + parsing[2] - 1,
+                };
+                self.pages.push(page);
+            }
+        }
+    }
+
+    fn find_lowest_location(&self, seed: u64) -> u64 {
+        let mut map_to_find = Mapping::SeedToSoil;
+        let mut next_dest = seed;
+        while map_to_find != Mapping::Finished {
+            let mut maybe_source: Vec<Option<u64>> = self
+                .pages
+                .iter()
+                .filter(|x| x.mapping == map_to_find)
+                .map(|x| x.destination_value(next_dest))
+                .filter(|x| x.is_some())
+                .collect();
+            if !maybe_source.is_empty() {
+                let temp = maybe_source.pop();
+                next_dest = temp.unwrap().unwrap();
+            }
+            map_to_find = map_to_find.next();
+        }
+        next_dest
+    }
+
+    pub fn part_one(&self) -> u64 {
+        let mut results = Vec::new();
+        self.seeds
+            .par_iter()
+            .map(|x| self.find_lowest_location(*x))
+            .collect_into_vec(&mut results);
+        results.sort();
+        *results.first().unwrap_or(&0)
+    }
+
+    pub fn part_two(&self) -> u64 {
+        let mut all_seeds = Vec::new();
+        let mut results = Vec::new();
+        for w in self.seeds.windows(2) {
+            for i in w[0]..w[1] {
+                all_seeds.push(i);
+            }
+        }
+        all_seeds
+            .par_iter()
+            .map(|x| self.find_lowest_location(*x))
+            .collect_into_vec(&mut results);
+        results.sort();
+        *results.first().unwrap_or(&0)
+    }
+}
+
+pub fn run(input: String) {
+    let mut almanac = Almanac::new();
+    almanac.fill(input);
+    // let lowest = almanac.part_one();
+    // println!("The lowest location is {lowest}");
+    let actual_lowest = almanac.part_two();
+    println!("The real lowest location is {actual_lowest}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use aoc2023::{day::Day::*, fileload, four, one, three, two};
+use aoc2023::{day::Day::*, fileload, five, four, one, three, two};
 use std::{env, process::ExitCode};
 
 fn main() -> ExitCode {
@@ -22,7 +22,7 @@ fn main() -> ExitCode {
         Two => two::run(input_buffer),
         Three => three::run(input_buffer),
         Four => four::run(input_buffer),
-        // Five => five::run(input_buffer),
+        Five => five::run(input_buffer),
         // Six => six::run(input_buffer),
         // Seven => seven::run(input_buffer),
         // Eight => eight::run(input_buffer),
