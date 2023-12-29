49f7a2e8c045eb5f14d5049f9a185248d0be1fa6
hafrei <h.a.freiburger@gmail.com>
2023-12-29 11:33:40 -0500

    Cargo fmt

--- src/nine.rs
+++ src/nine.rs
@@ -12,28 +12,28 @@ pub fn run(input: String) {
     for d in dataset {
         let current_len = d.len();
         let top_forecaset = d.last().expect("Outer dataset is empty");
-        let extrapolated = top_forecaset + calculate_prediction(&d, current_len); 
+        let extrapolated = top_forecaset + calculate_prediction(&d, current_len);
         forecast.push(extrapolated);
     }
     // println!("Forecast: {forecast:?}");
     let res: i32 = forecast.iter().sum();
-    println!("Sum is {res}");  //1889451523 is too high
+    println!("Sum is {res}"); //1889451523 is too high
 }
 
 fn calculate_prediction(consideration: &Vec<i32>, expected_len: usize) -> i32 {
     let mut direction: Option<Direction> = None;
-        let mut res = Vec::with_capacity(expected_len);
-        for c in consideration.windows(2) {
-            if c[0] > c[1] {
-                direction = Some(Direction::Decrease);
-                let sub = c[0] - c[1];
-                res.push(sub);
-            } else {
-                direction = Some(Direction::Increase);
-                let sub = c[1] - c[0];
-                res.push(sub);
-            }
+    let mut res = Vec::with_capacity(expected_len);
+    for c in consideration.windows(2) {
+        if c[0] > c[1] {
+            direction = Some(Direction::Decrease);
+            let sub = c[0] - c[1];
+            res.push(sub);
+        } else {
+            direction = Some(Direction::Increase);
+            let sub = c[1] - c[0];
+            res.push(sub);
         }
+    }
 
     let previous_data = res.last().expect("Somehow result vec has no data");
 
